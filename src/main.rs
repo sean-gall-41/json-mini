9226b2503bc1f3506b5f2e8cf79e2756e6f54cac
Sean Gallogly <galloglyst149@gmail.com>
2023-06-19 13:05:26 -0500

    cleaned up main :-)

--- src/main.rs
+++ src/main.rs
@@ -38,12 +38,6 @@ fn main() -> Result<(), Error> {
     });
     println!("{}", pretty_json);
 
-    //let input = String::from(r#"{"field_1": {"inner_field_1": "inner_value_1"}, "field_2": [true,false,true], "field_3":true}"#);
-    //let pretty = prettify_json(input).unwrap_or_else(|err| {
-    //    eprintln!("something went wrong while prettifying");
-    //    process::exit(1);
-    //});
-    //println!("{pretty}");
     Ok(())
 }
 
